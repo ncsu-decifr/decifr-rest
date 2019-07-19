413ca9236237ada30501b6c7468d8a96b63b0289
jim <jbwhite2@ncus.edu>
2019-07-18 22:20:27 -0400

    add flask app

---
+++ decifr-rest.py
@@ -0,0 +1,15 @@
+#!/usr/local/pythonenvs/decifr-rest/bin/python
+
+from flask import Flask, escape, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    name = request.args.get("name", "World")
+    return f'Hello, {escape(name)}!'
+
+
+if __name__ == '__main__':
+    export FLASK_APP=hello
