f46c4355eba9f10c0a3dc15e4ebff2c29181272a
jim white <jbwhite2@ncsu.edu>
2019-07-25 15:50:38 -0400

    add auth functions

--- decifr-rest.py
+++ decifr-rest.py
@@ -1,13 +1,40 @@
 #!/usr/local/pythonenvs/decifr-rest/bin/python
 
-from flask import Flask, escape, request
+from flask import Flask, escape, request, Response
 from flask import render_template
 from werkzeug.serving import run_simple
 import glob
+from functools import wraps
 
 app = Flask(__name__)
 app.config['TMP_FOLDER'] = "/var/www/html/tbas2_1/tmp"
 app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+        'Could not verify your access level for that URL.\n'
+        'You have to login with proper credentials', 401,
+        {'WWW-Authenticate': 'Basic realm="Login Required"'}
+    )
+
+
+def check_auth(username, password):
+    if username == 'admin' and password == 'secret':
+        return True
+    else:
+        return False
 
 
 @app.route('/')
@@ -32,6 +59,7 @@ def rest():
 
 
 @app.route("/runs/<runid>")
+@requires_auth
 def runs(runid):
     return render_template(
         'run.xml',
