473da59d25ca0729165628c1d0223b13e6f757da
jim white <jbwhite2@ncsu.edu>
2019-07-26 16:24:07 -0400

    develop script

--- scripts/get_leaves.py
+++ scripts/get_leaves.py
@@ -5,6 +5,7 @@ import sys
 from lxml import etree
 from lxml.etree import XMLParser
 from lxml.etree import parse
+import json
 
 
 TMP_FOLDER = "/var/www/html/tbas2_1/tmp"
@@ -17,13 +18,28 @@ def main(run_id):
     p = XMLParser(huge_tree=True)
     tree = parse(xmlfile, parser=p)
     print(tree)
+    root = tree.getroot()
+    name_list = []
+
+    for x in root:
+        if x.tag == '{http://www.phyloxml.org}phylogeny':
+            phylogeny = x
+
+    for cnt, element in enumerate(
+        phylogeny.iter('{http://www.phyloxml.org}name')
+    ):
+        name_list.append(element.text)
+        if cnt > 5000:
+            break
+    return json.dumps(name_list, indent=4)
 
 
 
 if __name__ == '__main__':
     try:
         run_id = sys.argv[1]
-        main(run_id)
+        retval = main(run_id)
+        print(retval)
     except Exception:
         error = traceback.format_exc()
         print(error)
