63b9c1eb2ea0b8697e96d4f5ac765255279ba2e2
jim white <jbwhite2@ncsu.edu>
2019-07-26 15:58:10 -0400

    add script

---
+++ scripts/get_leaves.py
@@ -0,0 +1,15 @@
+#!/usr/local/pythonenvs/decifr-rest/bin/python
+
+import traceback
+
+def main():
+    raise Exception("dev")
+
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception:
+        error = traceback.format_exc()
+        print error
