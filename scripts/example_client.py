607f2661984f69f9d7e0941f858245ce1763afe5
jim <jbwhite2@ncsu.edu>
2024-10-23 15:08:13 -0400

    example client

--- scripts/example_client.py
+++ scripts/example_client.py
@@ -4,17 +4,26 @@
 
 import requests
 import sys
+import os
 
 SERVER = 'https://rest.cifr.ncsu.edu/'
 USERNAME = 'admin'
 PASSWORD = 'secret'
+LOCAL_DIRECTORY = '/tmp/'
 
-def main():
-    pass
+def main(run_id):
+    print("getting data for %s" % run_id)
+    work_dir = "%s/decifr-rest%s" % (LOCAL_DIRECTORY, run_id)
+    os.makedirs(work_dir, exist_ok = True)
+
+    leaves_url = "%s/leaves/%s?return_type=json" % (SERVER, run_id)
+    r = requests.get(leaves_url, auth=(USERNAME, PASSWORD))
+    print(r.text)
+
 
 if __name__ == '__main__':
     try:
         run_id = sys.argv[1]
     except IndexError:
         print("usage: python example_client.py <run_id>")
-    main()
+    main(run_id)
