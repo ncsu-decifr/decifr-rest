e36e14db79a5fec430e4685065d6704ea2fd2f77
jim <jbwhite2@ncsu.edu>
2024-10-23 20:11:25 -0400

    example client

--- scripts/example_client.py
+++ scripts/example_client.py
@@ -13,7 +13,7 @@ PASSWORD = 'password'
 LOCAL_DIRECTORY = '/tmp/'
 
 def leaves(run_id):
-    print("getting data for %s" % run_id)
+    print("getting leaves for %s" % run_id)
     work_dir = "%s/decifr-rest%s" % (LOCAL_DIRECTORY, run_id)
     os.makedirs(work_dir, exist_ok = True)
     os.chdir(work_dir)
@@ -22,7 +22,7 @@ def leaves(run_id):
     r = requests.get(leaves_url, auth=(USERNAME, PASSWORD))
     print(r.status_code)
     if r.status_code != 200:
-        print("imvalid runid or server error")
+        print("invalid runid or server error")
         sys.exit()
 
     with open("leaves.json", "w") as fp:
@@ -45,7 +45,36 @@ def leaves(run_id):
                 fp.write(r.text)
 
 def queries(run_id):
-    pass
+    print("getting queries for %s" % run_id)
+    work_dir = "%s/decifr-rest%s" % (LOCAL_DIRECTORY, run_id)
+    os.makedirs(work_dir, exist_ok = True)
+    os.chdir(work_dir)
+
+    queries_url = "%s/queries/%s?return_type=json" % (SERVER, run_id)
+    r = requests.get(queries_url, auth=(USERNAME, PASSWORD))
+    print(r.status_code)
+    if r.status_code != 200:
+        print("invalid runid or server error")
+        sys.exit()
+
+    with open("queries.json", "w") as fp:
+        fp.write(r.text)
+
+    os.makedirs("queries", exist_ok = True)
+    queries_json = r.text
+    queries = json.loads(queries_json)
+
+    for query in queries:
+        query_url = "%s/query/%s/metadata?query=%s&return_type=json" % (
+            SERVER, run_id, query
+        )
+        r = requests.get(query_url, auth=(USERNAME, PASSWORD))
+        if r.status_code != 200:
+            continue
+        else:
+            print(query)
+            with open("queries/%s.json" % query, "w") as fp:
+                fp.write(r.text)
 
 
 if __name__ == '__main__':
